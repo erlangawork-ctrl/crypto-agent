147711474284e23ec34f99a772598bcc15530dbb
erlangawork-ctrl <erlanga.work@gmail.com>
2026-09-26 01:41:39 +0200

    Update agent.py

--- agent.py
+++ agent.py
@@ -679,7 +679,7 @@ def run_scanner():
             else:
                 print(
                     f'[{now_str}] [{symbol}] Scan voltooid -> NO-GO / Geen valide S/R'
-                    ' angular setup.',
+                    ' setup.',
                     flush=True,
                 )
 
